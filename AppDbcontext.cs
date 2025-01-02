ee93521ee5859e03a4faa8b8ed93a0123b73475c
ValiITSchool <vali.bernat@gmail.com>
2025-01-02 15:01:38 +0200

    master push(afisare studenti ok)

--- AppDbcontext.cs
+++ AppDbcontext.cs
@@ -1,4 +1,6 @@
-﻿namespace WebApplication1
+﻿using WebApplication1.Models;
+
+namespace WebApplication1
 {
     using Microsoft.EntityFrameworkCore;
 
@@ -11,9 +13,28 @@
         public DbSet<Cazare> Cazari { get; set; }
         public DbSet<Administrator> Administratori { get; set; }
         public DbSet<Log> Loguri { get; set; }
+        
+        // Add a DbSet for CazareInfo
+        public DbSet<CazareInfo> CazareInfos { get; set; } // Required to query CazareInfo
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure CazareInfo as a keyless entity
+            modelBuilder.Entity<CazareInfo>().HasNoKey();
+        }
     }
+
+    // CazareInfo class definition (make sure this matches your SQL query result)
+    // public class CazareInfo
+    // {
+    //     public string NumeCamin { get; set; }  // Maps to the "C.Nume" column in your SQL
+    //     public int NumarCamera { get; set; }  // Maps to "Cam.NumarCamera"
+    //     public int NumarColegi { get; set; }  // Maps to "NumarColegi"
+    // }
 }
