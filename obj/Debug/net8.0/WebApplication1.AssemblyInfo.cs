ac4f7009f8b8d038209f71c8c227f28441a89783
ValiITSchool <vali.bernat@gmail.com>
2025-01-03 21:52:43 +0200

    full commit

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cea7d050c214b0fc20e41f5cf8e3e0374adc0b0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d358a5b99411138ff3e1b765ffc2a65382313b7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
