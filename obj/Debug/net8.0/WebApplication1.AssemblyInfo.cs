b3c870fa35343bc6b49b35833ac9c65a5c1d4696
ValiITSchool <vali.bernat@gmail.com>
2025-01-05 18:01:05 +0200

    push+buton de afisare studenti

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7c22eb5a071a57b57ddde6deea266a686f2e070")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5dffddc278eadbe52c2afac029620c7a30cc502")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
