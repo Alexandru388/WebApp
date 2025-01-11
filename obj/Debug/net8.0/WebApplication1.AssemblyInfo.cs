fa5277d2a4e552dc92b6b1d4b63a547c80dd9892
ValiITSchool <vali.bernat@gmail.com>
2025-01-11 18:02:45 +0200

    push

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3c870fa35343bc6b49b35833ac9c65a5c1d4696")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a1e9c9d9fb245c7a31bf327b076680c709bf17")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
