2f54a6c0698f8cbf0e922282eb357a0472de46d1
Avram <avramandrei2149@gmail.com>
2025-01-14 19:25:30 +0200

    Am terminat cu folderul Administrator

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e41d6657274bebcbd669c6e6ea41889e047fe209")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9e65eacc0d8e5c8f120397b93169b6dc91bea4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
