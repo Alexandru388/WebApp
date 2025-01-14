ab50be3c92542dc76442a9cbed5c75f31d207df4
Avram <avramandrei2149@gmail.com>
2025-01-14 20:14:26 +0200

    am finalizat design ul la folderul student

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9e65eacc0d8e5c8f120397b93169b6dc91bea4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1b2a964b76f1034539c3a33a27c89f233a0126e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
