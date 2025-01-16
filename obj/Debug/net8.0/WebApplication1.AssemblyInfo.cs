9efc940c905c22bf38a084e6a2f3eb32531faab8
ValiITSchool <vali.bernat@gmail.com>
2025-01-16 17:13:55 +0200

    adaugare design forntend

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a1e9c9d9fb245c7a31bf327b076680c709bf17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a7a506b64f6193d9495321fe504457eeb22d8b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
